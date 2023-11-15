3f31b9f1830e7edf42d68c7c3a86343808089db9
깃chengsukim <kcs981123@naver.com>
2023-11-15 11:45:06 +0900

    sbb1

---
+++ src/main/java/com/sdd/sbb1/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.sdd.sbb1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface AnswerRepository extends JpaRepository<Answer , Integer> {
+
+}
--- src/test/java/com/sdd/sbb1/Sbb1ApplicationTests.java
+++ src/test/java/com/sdd/sbb1/Sbb1ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.sdd.sbb1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootTest
 class Sbb1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository questionRepository;
 
+	@Test
+	void testJpa() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
 }
